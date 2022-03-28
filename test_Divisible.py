a33d5ec7131c818dff0c2053daa3789b9f15fc13
likhithar2507 <73028560+likhithar2507@users.noreply.github.com>
2022-03-28 12:27:22 +0530

    Update test_Divisible.py

--- test_Divisible.py
+++ test_Divisible.py
@@ -6,16 +6,16 @@ def test_divisible_5():
     assert result == True
 
 def test_divisible_7():
-    num=3
+    num=88
     result=Divisible.divisible_7(num)
     assert result == False
 
 def test_divisible_9():
-    num=9
+    num=97
     result=Divisible.divisible_9(num)
     assert result == True
 
 def test_divisible_10():
-    num=8
+    num=80
     result=Divisible.divisible_10(num)
     assert result == False
